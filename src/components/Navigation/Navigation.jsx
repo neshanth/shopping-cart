b3a860f633b4d262affb1efc8879c7157345808a
Neshanth <m.neshanth13@gmail.com>
2021-12-13 22:25:56 +0530

    added logo and icons

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -2,20 +2,27 @@ import React from "react";
 import { useGloablContext } from "../../context/context";
 import "./navigation.css";
 import { Link } from "react-router-dom";
+import { AiFillThunderbolt } from "react-icons/ai";
+import { CgShoppingCart } from "react-icons/cg";
 
 function Navigation() {
   const { count } = useGloablContext();
 
   return (
     <>
-      <div className="navigation">
-        <Link to="/">Logo</Link>
+      <div className="navigation container">
+        <Link to="/">
+          <AiFillThunderbolt size={50} color="#2189ff" />
+          <span className="logo-title">Estore</span>
+        </Link>
 
         <ul className="navigation-container">
           <li className="menu-item">
-            <Link to="/cart">Cart</Link>
-            <span>{count}</span>
+            <Link to="/cart">
+              <CgShoppingCart size={25} color="#2189ff" />
+            </Link>
           </li>
+          <p className="cart-count">{count}</p>
         </ul>
       </div>
     </>
